961bbeae8f1ee8aee6cee39053532f0b797d4251
Afonso Gloeden <afgloeden@gmail.com>
2018-12-31 19:22:56 -0200

    Issue #8 | JS added in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,17 @@
 const mix = require('laravel-mix');
 
-mix.sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
+   .extract([
+      'jquery',
+      'popper.js',
+      'bootstrap',
+      '@fortawesome/fontawesome-free/js/solid.js',
+      '@fortawesome/fontawesome-free/js/regular.js',
+      '@fortawesome/fontawesome-free/js/brands.js',
+      '@fortawesome/fontawesome-free/js/fontawesome.js'
+   ]);
 
 if (mix.inProduction()) {
-    mix.version();
+   mix.version();
 }
