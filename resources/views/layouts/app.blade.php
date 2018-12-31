36f22d3f02bca41b7477e5b7c4034a03478e7a40
Afonso Gloeden <afgloeden@gmail.com>
2018-12-31 19:23:21 -0200

    Issue #8 | JS added in view layouts.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,5 +42,8 @@
         @endif
         @yield('content')
     </main>
+    <script type="text/javascript" src="{{ mix('/js/manifest.js') }}"></script>
+    <script type="text/javascript" src="{{ mix('/js/vendor.js') }}"></script>
+    <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
