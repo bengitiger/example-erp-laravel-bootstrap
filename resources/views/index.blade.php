4538848dd4f6e08bf846e8a08cdef6f85f056a2f
Afonso Gloeden <afgloeden@gmail.com>
2019-01-02 17:22:08 -0200

    Issue #8 | View index added

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="text-center m-5">{{ config('app.name') }}</h1>
+<p class="text-center m-5 text-danger">Website in development.</p>
+@endsection
