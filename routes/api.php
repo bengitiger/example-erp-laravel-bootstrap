6fecd6ce499a92877a57fbad3ff951ec57fef18d
Afonso Gloeden <afgloeden@gmail.com>
2019-01-02 17:27:55 -0200

    Default comment removed from routes/api.php file

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,7 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 // This function causes problems with the "php artisan route:cache" command.
-/*Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});*/
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
