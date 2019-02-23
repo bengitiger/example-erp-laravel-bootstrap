8f1232676f17a14fc56f7125c7758d1f0bef082c
Afonso Gloeden <afgloeden@gmail.com>
2019-02-23 16:57:22 -0300

    Issue #8 | purifyCss added and indentation updated in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,23 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .extract([
-      'jquery',
-      'popper.js',
-      'bootstrap',
-      '@fortawesome/fontawesome-free/js/solid.js',
-      '@fortawesome/fontawesome-free/js/regular.js',
-      '@fortawesome/fontawesome-free/js/brands.js',
-      '@fortawesome/fontawesome-free/js/fontawesome.js'
-   ]);
+mix
+  .js('resources/js/app.js',       'public/js')
+  .sass('resources/sass/app.scss', 'public/css')
+  .extract([
+    'jquery',
+    'popper.js',
+    'bootstrap',
+    '@fortawesome/fontawesome-free/js/solid.js',
+    '@fortawesome/fontawesome-free/js/regular.js',
+    '@fortawesome/fontawesome-free/js/brands.js',
+    '@fortawesome/fontawesome-free/js/fontawesome.js'
+  ]);
 
 if (mix.inProduction()) {
-   mix.version();
+  mix
+    .version()
+    .sourceMaps()
+    .options({
+      purifyCss: true
+    });
 }
