99f3fd201ca296cf81f2a56bed9552e7630ea8f5
Afonso Gloeden <afgloeden@gmail.com>
2019-02-24 22:14:23 -0300

    Issue #1 | database/migrations/2019_02_25_003919_create_customers_table.php added

---
+++ database/migrations/2019_02_25_003919_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('cellphone');
+            $table->string('address');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
