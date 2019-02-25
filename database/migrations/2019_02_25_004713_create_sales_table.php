ed720a72ea32e0b3a64ccf8d82f953a2d946d8b8
Afonso Gloeden <afgloeden@gmail.com>
2019-02-24 22:15:13 -0300

    Issue #1 | database/migrations/2019_02_25_004713_create_sales_table.php added

---
+++ database/migrations/2019_02_25_004713_create_sales_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->integer('id');
+            $table->unsignedInteger('product_id');
+            $table->integer('amount');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+
+            $table
+                ->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
