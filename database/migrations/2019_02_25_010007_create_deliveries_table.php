fafd4ed20c39e13c8a2cea08157ffedbc7e37ba8
Afonso Gloeden <afgloeden@gmail.com>
2019-02-24 22:15:53 -0300

    Issue #1 | database/migrations/2019_02_25_010007_create_deliveries_table.php added

---
+++ database/migrations/2019_02_25_010007_create_deliveries_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->unsignedInteger('cart_id');
+            $table->unsignedInteger('customer_id');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+
+            $table
+                ->foreign('cart_id')
+                ->references('id')
+                ->on('carts')
+                ->onDelete('cascade');
+            $table
+                ->foreign('customer_id')
+                ->references('id')
+                ->on('customers')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+}
