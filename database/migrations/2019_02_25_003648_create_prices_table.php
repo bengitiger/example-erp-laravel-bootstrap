f64013804b4081f15cac9adf7cdd26aaa32776cb
Afonso Gloeden <afgloeden@gmail.com>
2019-02-24 22:14:12 -0300

    Issue #1 | database/migrations/2019_02_25_003648_create_prices_table.php added

---
+++ database/migrations/2019_02_25_003648_create_prices_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->unsignedInteger('product_id');
+            $table->float('value', 7, 2);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+
+            $table
+                ->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
