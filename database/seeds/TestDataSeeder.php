9e40f955ee3b21c81dcd0ec9546df08c88f7f60b
Afonso Gloeden <afgloeden@gmail.com>
2019-02-25 19:59:26 -0300

    Issue #4 | database/seeds/TestDataSeeder.php added

---
+++ database/seeds/TestDataSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TestDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Users
+        factory(App\Models\User::class, 10000)->create();
+    }
+}
