042ae2e3ac1bdd6fe08cd281ed58ea8050784317
Afonso Gloeden <afgloeden@gmail.com>
2019-02-24 22:15:41 -0300

    Issue #1 | database/migrations/2019_02_25_005657_create_expenses_table.php added

---
+++ database/migrations/2019_02_25_005657_create_expenses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->text('description');
+            $table->float('value', 8, 2);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
