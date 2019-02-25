22a1561a9aee185b25d1c901e0a443eb38023a83
Afonso Gloeden <afgloeden@gmail.com>
2019-02-24 22:13:31 -0300

    Issue #1 | database/migrations/2019_02_24_233805_create_salaries_table.php added

---
+++ database/migrations/2019_02_24_233805_create_salaries_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->unsignedInteger('role_id');
+            $table->float('value', 7, 2);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+
+            $table
+                ->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salaries');
+    }
+}
