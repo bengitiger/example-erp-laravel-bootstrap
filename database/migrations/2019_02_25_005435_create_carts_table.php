5c2db0ef19991da33aeb031bd91729a7917b7cea
Afonso Gloeden <afgloeden@gmail.com>
2019-02-24 22:15:30 -0300

    Issue #1 | database/migrations/2019_02_25_005435_create_carts_table.php added

---
+++ database/migrations/2019_02_25_005435_create_carts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->unsignedInteger('employee_id');
+            $table->unsignedInteger('sale_id');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+
+            $table
+                ->foreign('employee_id')
+                ->references('id')
+                ->on('employees')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
