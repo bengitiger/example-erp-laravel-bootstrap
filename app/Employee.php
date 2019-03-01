e976a85e45393d1f46310b86b9efca248f637fae
Afonso Gloeden <afgloeden@gmail.com>
2019-03-01 20:33:15 -0300

    Issue #2 | app/Employee.php updated

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,73 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table    = 'employees';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'start_date',
+        'end_date',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded  = [
+        'id',
+        'user_id',
+        'role_id',
+        'store_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps  = false;
+
+    /**
+     * Get the user that owns the employee.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+
+    /**
+     * Get the role that owns the employee.
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Role', 'role_id', 'id');
+    }
+
+    /**
+     * Get the store that owns the employee.
+     */
+    public function store()
+    {
+        return $this->belongsTo('App\Store', 'store_id', 'id');
+    }
+
+    /**
+     * Get the carts for the employee.
+     */
+    public function carts()
+    {
+        return $this->hasMany('App\Cart', 'employee_id', 'id');
+    }
 }
