34bf7bbaa8b73065948fbd96d52f3b25e5935729
Afonso Gloeden <afgloeden@gmail.com>
2019-03-01 20:33:24 -0300

    Issue #2 | app/Expense.php updated

--- app/Expense.php
+++ app/Expense.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table    = 'expenses';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'description',
+        'value',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded  = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps  = false;
 }
