1e36a0ea7f7f2043ca7775955ad17678c09c44b8
Afonso Gloeden <afgloeden@gmail.com>
2019-03-01 20:35:11 -0300

    Issue #2 | app/Sale.php updated

--- app/Sale.php
+++ app/Sale.php
@@ -6,5 +6,54 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table    = 'sales';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'amount',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded  = [
+        'id',
+        'product_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps  = false;
+
+    /**
+     * Get the product that owns the sale.
+     */
+    public function product()
+    {
+        return $this->belongsTo('App\Product', 'product_id', 'id');
+    }
+
+    /**
+     * Get the cart associated with the sale.
+     */
+    public function cart()
+    {
+        return $this->hasOne('App\Cart', 'sale_id', 'id');
+    }
 }
