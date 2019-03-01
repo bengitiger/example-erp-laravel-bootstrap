1b75dd7d402772f0e39fa90d3e713717939d1f25
Afonso Gloeden <afgloeden@gmail.com>
2019-03-01 20:33:48 -0300

    Issue #2 | app/Product.php updated

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,63 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table    = 'products';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'stock',
+        'low_quantity',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded  = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps  = false;
+
+    /**
+     * Get the purchases for the product.
+     */
+    public function purchases()
+    {
+        return $this->hasMany('App\Purchase', 'product_id', 'id');
+    }
+
+    /**
+     * Get the sales for the product.
+     */
+    public function sales()
+    {
+        return $this->hasMany('App\Sale', 'product_id', 'id');
+    }
+
+    /**
+     * Get the price associated with the product.
+     */
+    public function price()
+    {
+        return $this->hasOne('App\Price', 'product_id', 'id');
+    }
 }
