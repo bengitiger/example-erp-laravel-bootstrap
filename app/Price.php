b20e53754d8755c88a2935425967a2607db65e94
Afonso Gloeden <afgloeden@gmail.com>
2019-03-01 20:33:34 -0300

    Issue #2 | app/Price.php updated

--- app/Price.php
+++ app/Price.php
@@ -6,5 +6,46 @@ use Illuminate\Database\Eloquent\Model;
 
 class Price extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table    = 'prices';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'value',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded  = [
+        'id',
+        'product_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps  = false;
+
+    /**
+     * Get the product that owns the price.
+     */
+    public function product()
+    {
+        return $this->belongsTo('App\Product', 'product_id', 'id');
+    }
 }
