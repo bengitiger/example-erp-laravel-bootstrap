8beade6d07581889b704882fccbc219d55492302
Afonso Gloeden <afgloeden@gmail.com>
2019-03-01 20:32:46 -0300

    Issue #2 | app/Customer.php updated

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,49 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table    = 'customers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'cellphone',
+        'address',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded  = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps  = false;
+
+    /**
+     * Get the deliveries for the customer.
+     */
+    public function deliveries()
+    {
+        return $this->hasMany('App\Delivery', 'customer_id', 'id');
+    }
 }
