618fd55708dbd7b5ed9c2d2353b70495af1d8e3c
Afonso Gloeden <afgloeden@gmail.com>
2019-03-01 20:35:21 -0300

    Issue #2 | app/Store.php updated

--- app/Store.php
+++ app/Store.php
@@ -6,5 +6,56 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table    = 'stores';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'address',
+        'phone',
+        'home',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded  = [
+        'id',
+        'company_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps  = false;
+
+    /**
+     * Get the company that owns the store.
+     */
+    public function company()
+    {
+        return $this->belongsTo('App\Company', 'company_id', 'id');
+    }
+
+    /**
+     * Get the employees for the store.
+     */
+    public function employees()
+    {
+        return $this->hasMany('App\Employee', 'store_id', 'id');
+    }
 }
