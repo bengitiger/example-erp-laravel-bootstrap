2f07623ac431e2ae0c1d85a6c78fe37d944c356a
Afonso Gloeden <afgloeden@gmail.com>
2019-03-01 20:35:02 -0300

    Issue #2 | app/Salary.php updated

--- app/Salary.php
+++ app/Salary.php
@@ -6,5 +6,46 @@ use Illuminate\Database\Eloquent\Model;
 
 class Salary extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table    = 'salaries';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'value',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded  = [
+        'id',
+        'role_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps  = false;
+
+    /**
+     * Get the role that owns the salary.
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Role', 'role_id', 'id');
+    }
 }
