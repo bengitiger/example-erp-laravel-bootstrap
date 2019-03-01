8b90aee8323f8a25166669dbbbad006c95c679e4
Afonso Gloeden <afgloeden@gmail.com>
2019-03-01 20:34:44 -0300

    Issue #2 | app/Purchase.php updated

--- app/Purchase.php
+++ app/Purchase.php
@@ -6,5 +6,56 @@ use Illuminate\Database\Eloquent\Model;
 
 class Purchase extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table    = 'purchases';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'amount',
+        'price_unit',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded  = [
+        'id',
+        'product_id',
+        'supplier_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps  = false;
+
+    /**
+     * Get the product that owns the purchase.
+     */
+    public function product()
+    {
+        return $this->belongsTo('App\Product', 'product_id', 'id');
+    }
+
+    /**
+     * Get the supplier that owns the purchase.
+     */
+    public function supplier()
+    {
+        return $this->belongsTo('App\Supplier', 'supplier_id', 'id');
+    }
 }
