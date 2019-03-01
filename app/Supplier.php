65e8bc97b74c6159ee3deaf34936ca3d04d5fe4e
Afonso Gloeden <afgloeden@gmail.com>
2019-03-01 20:35:30 -0300

    Issue #2 | app/Supplier.php updated

--- app/Supplier.php
+++ app/Supplier.php
@@ -6,5 +6,48 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table    = 'suppliers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded  = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps  = false;
+
+    /**
+     * Get the purchases for the supplier.
+     */
+    public function purchases()
+    {
+        return $this->hasMany('App\Purchase', 'supplier_id', 'id');
+    }
 }
