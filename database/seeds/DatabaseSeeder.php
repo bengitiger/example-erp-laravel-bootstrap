526f33cdfdc672fdba290aae3f7f084499764b5e
Afonso Gloeden <afgloeden@gmail.com>
2019-03-02 20:56:56 -0300

    Issue #4 |Carbon functions updated in database/seeds/DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,5 @@
 <?php
 
-use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
@@ -17,43 +16,43 @@ class DatabaseSeeder extends Seeder
         $idRoleAdmin = DB::table('roles')->insertGetId([
             'name'       => 'Administrator',
             'slug'       => 'admin',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
 
         DB::table('roles')->insert([
             'name'       => 'Information Technology',
             'slug'       => 'it',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
 
         DB::table('roles')->insert([
             'name'       => 'Financial',
             'slug'       => 'financial',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
 
         DB::table('roles')->insert([
             'name'       => 'Human Resources',
             'slug'       => 'hr',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
 
         DB::table('roles')->insert([
             'name'       => 'Manager',
             'slug'       => 'manager',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
 
         DB::table('roles')->insert([
             'name'       => 'Salesman',
             'slug'       => 'salesman',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
 
         // Administrator
@@ -63,14 +62,14 @@ class DatabaseSeeder extends Seeder
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token'    => str_random(10),
-            'created_at'        => Carbon::now(),
-            'updated_at'        => Carbon::now()
+            'created_at'        => now(),
+            'updated_at'        => now()
         ]);
 
         $idCompany = DB::table('companies')->insertGetId([
             'name'       => 'Main Company',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
 
         $idStore = DB::table('stores')->insertGetId([
@@ -78,18 +77,18 @@ class DatabaseSeeder extends Seeder
             'address'    => 'Main address, 00',
             'phone'      => '000',
             'home'       => true,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
 
         $idStore = DB::table('employees')->insertGetId([
             'user_id'    => $idUserAdmin,
             'role_id'    => $idRoleAdmin,
             'store_id'   => $idStore,
-            'start_date' => Carbon::now(),
-            'end_date'   => Carbon::now()->addYear(20),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
+            'start_date' => now(),
+            'end_date'   => now()->addYear(20),
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
     }
 }
