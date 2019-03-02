4850368ba6251f211c551619d70d868a7c4509db
Afonso Gloeden <afgloeden@gmail.com>
2019-03-02 20:58:26 -0300

    Issue #4 | Run function updated in database/seeds/TestDataSeeder.php

--- database/seeds/TestDataSeeder.php
+++ database/seeds/TestDataSeeder.php
@@ -11,7 +11,19 @@ class TestDataSeeder extends Seeder
      */
     public function run()
     {
-        // Users
-        factory(App\User::class, 10000)->create();
+        factory(App\User::class,     100 )->create();
+        factory(App\Company::class,  1   )->create();
+        factory(App\Store::class,    15  )->create();
+        factory(App\Employee::class, 200 )->create();
+        factory(App\Product::class,  500 )->create();
+        factory(App\Sale::class,     1000)->create();
+        factory(App\Cart::class,     400 )->create();
+        factory(App\Customer::class, 400 )->create();
+        factory(App\Delivery::class, 250 )->create();
+        factory(App\Expense::class,  100 )->create();
+        factory(App\Price::class,    200 )->create();
+        factory(App\Supplier::class, 50  )->create();
+        factory(App\Purchase::class, 500 )->create();
+        factory(App\Salary::class,   50  )->create();
     }
 }
