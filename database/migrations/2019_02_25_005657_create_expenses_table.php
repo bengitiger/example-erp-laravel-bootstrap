d5c457eef140d4f6d4b78ee47ece77529877e420
Afonso Gloeden <afgloeden@gmail.com>
2019-03-02 20:55:25 -0300

    Issue #1 | Id type updated to uuid in database/migrations/2019_02_25_005657_create_expenses_table.php

--- database/migrations/2019_02_25_005657_create_expenses_table.php
+++ database/migrations/2019_02_25_005657_create_expenses_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateExpensesTable extends Migration
 {
@@ -14,12 +15,16 @@ class CreateExpensesTable extends Migration
     public function up()
     {
         Schema::create('expenses', function (Blueprint $table) {
-            $table->bigIncrements('id')->unique();
+            $table->uuid('id')->unique();
             $table->text('description');
             $table->float('value', 8, 2);
             $table->timestamp('created_at');
             $table->timestamp('updated_at');
+
+            $table->primary('id');
         });
+
+        DB::statement('ALTER TABLE expenses ALTER COLUMN id SET DEFAULT uuid_generate_v4();');
     }
 
     /**
