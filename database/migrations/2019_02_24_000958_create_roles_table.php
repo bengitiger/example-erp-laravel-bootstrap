b625dd9bf0750081ea1f7b201ff735626767ad3e
Afonso Gloeden <afgloeden@gmail.com>
2019-03-02 20:51:18 -0300

    Issue #1 | Id type updated to uuid in database/migrations/2019_02_24_000958_create_roles_table.php

--- database/migrations/2019_02_24_000958_create_roles_table.php
+++ database/migrations/2019_02_24_000958_create_roles_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateRolesTable extends Migration
 {
@@ -14,12 +15,16 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->bigIncrements('id')->unique();
+            $table->uuid('id')->unique();
             $table->string('name');
             $table->string('slug');
             $table->timestamp('created_at');
             $table->timestamp('updated_at');
+
+            $table->primary('id');
         });
+
+        DB::statement('ALTER TABLE roles ALTER COLUMN id SET DEFAULT uuid_generate_v4();');
     }
 
     /**
