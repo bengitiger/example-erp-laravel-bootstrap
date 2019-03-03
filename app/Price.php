37dbcdf92bd50ebb0c31e7ca393a6d0eee35d206
Afonso Gloeden <afgloeden@gmail.com>
2019-03-02 21:14:34 -0300

    Issue #2 | primaryKey, incrementing, keyType, hidden and casts variables added to app/Price.php

--- app/Price.php
+++ app/Price.php
@@ -11,14 +11,35 @@ class Price extends Model
      *
      * @var string
      */
-    protected $table    = 'prices';
+    protected $table      = 'prices';
+
+    /**
+     * The table primary key.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The increment of the primary key.
+     *
+     * @var bool
+     */
+    public $incrementing  = false;
+
+    /**
+     * The type of the primary key..
+     *
+     * @var string
+     */
+    protected $keyType    = 'string';
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
+    protected $fillable   = [
         'value',
     ];
 
@@ -27,7 +48,7 @@ class Price extends Model
      *
      * @var array
      */
-    protected $guarded  = [
+    protected $guarded    = [
         'id',
         'product_id',
         'created_at',
@@ -35,11 +56,27 @@ class Price extends Model
     ];
 
     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden     = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts      = [
+        'id' => 'string',
+    ];
+
+    /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
      */
-    public $timestamps  = false;
+    public $timestamps    = false;
 
     /**
      * Get the product that owns the price.
