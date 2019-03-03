e32fd561e7522c578b7a5f9ff742ee6f8126eca8
Afonso Gloeden <afgloeden@gmail.com>
2019-03-02 21:14:15 -0300

    Issue #2 | primaryKey, incrementing, keyType, hidden and casts variables added to app/Expense.php

--- app/Expense.php
+++ app/Expense.php
@@ -11,14 +11,35 @@ class Expense extends Model
      *
      * @var string
      */
-    protected $table    = 'expenses';
+    protected $table      = 'expenses';
+
+    /**
+     * The table primary key.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The increment of the primary key.
+     *
+     * @var bool
+     */
+    public $incrementing  = false;
+
+    /**
+     * The type of the primary key..
+     *
+     * @var string
+     */
+    protected $keyType    = 'string';
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
+    protected $fillable   = [
         'description',
         'value',
     ];
@@ -28,10 +49,26 @@ class Expense extends Model
      *
      * @var array
      */
-    protected $guarded  = [
+    protected $guarded    = [
         'id',
         'created_at',
         'updated_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden     = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts      = [
+        'id' => 'string',
     ];
 
     /**
@@ -39,5 +76,5 @@ class Expense extends Model
      *
      * @var bool
      */
-    public $timestamps  = false;
+    public $timestamps    = false;
 }
