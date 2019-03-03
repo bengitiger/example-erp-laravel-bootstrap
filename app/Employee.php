0899458dba2d1dc256951e00f044efac8eeea4a7
Afonso Gloeden <afgloeden@gmail.com>
2019-03-02 21:13:49 -0300

    Issue #2 | primaryKey, incrementing, keyType, hidden and casts variables added to app/Employee.php

--- app/Employee.php
+++ app/Employee.php
@@ -11,14 +11,35 @@ class Employee extends Model
      *
      * @var string
      */
-    protected $table    = 'employees';
+    protected $table      = 'employees';
+
+    /**
+     * The table primary key.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The increment of the primary key.
+     *
+     * @var bool
+     */
+    public $incrementing  = false;
+
+    /**
+     * The type of the primary key..
+     *
+     * @var string
+     */
+    protected $keyType    = 'string';
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
+    protected $fillable   = [
         'start_date',
         'end_date',
     ];
@@ -28,7 +49,7 @@ class Employee extends Model
      *
      * @var array
      */
-    protected $guarded  = [
+    protected $guarded    = [
         'id',
         'user_id',
         'role_id',
@@ -38,11 +59,27 @@ class Employee extends Model
     ];
 
     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden     = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts      = [
+        'id' => 'string',
+    ];
+
+    /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
      */
-    public $timestamps  = false;
+    public $timestamps    = false;
 
     /**
      * Get the user that owns the employee.
