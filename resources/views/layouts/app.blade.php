1a8788d66e9be508ad16917c9dd76c2253ed783f
Afonso Gloeden <afgloeden@gmail.com>
2019-03-02 21:06:26 -0300

    Issue #8 | asset() function added in CSS and JS files in resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield('title-app'){{ config('app.name') }}</title>
-    <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset(mix('/css/app.css')) }}"/>
     <link rel="icon" type="image/x-icon" href="{{ asset('/favicon.ico') }}" />
 </head>
 <body>
@@ -42,8 +42,8 @@
         @endif
         @yield('content')
     </main>
-    <script type="text/javascript" src="{{ mix('/js/manifest.js') }}"></script>
-    <script type="text/javascript" src="{{ mix('/js/vendor.js') }}"></script>
-    <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ asset(mix('/js/manifest.js')) }}"></script>
+    <script type="text/javascript" src="{{ asset(mix('/js/vendor.js')) }}"></script>
+    <script type="text/javascript" src="{{ asset(mix('/js/app.js')) }}"></script>
 </body>
 </html>
